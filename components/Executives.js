c5d56ed68cb6084fb8e69418d7a95a3f1cad59cc
Matthew <matgold17@gmail.com>
2021-12-14 19:35:14 +0100

    updated

--- components/Executives.js
+++ components/Executives.js
@@ -6,89 +6,162 @@ function Executives() {
                    IMUYA EXECUTIVES
                 </h1>
                 <div className="mt-6 text-bold text-lg tracking-wider text-center grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-8">
-                    <div className="mx-w-sm mx-auto border rounded-lg shadow-lg shadow-purple-400">
-                        <img src="/avatar.svg" />
-                        <div className="p-4">
-                            <p>xxxxxxxx xxxxxxxxx</p>
-                            <p>Youth President</p>
+                    
+                    <div className="w-full max-w-md mx-auto mt-8 px-4 space-y-32 rounded-lg border shadow-xl shadow-purple-300">
+                        <div className="relative">
+                            <div className="absolute w-36 h-36 bg-white border-l border-purple-600 rounded-full -left-8 -top-8 flex items-center justify-center shadow-md">
+                                <div className="w-32 h-32 rounded-full">
+                                    <img src="/avatar.svg"/>
+                                </div>
+                            </div>
                         </div>
+                        <div className="p-4 text-center">
+                                <p>XXXXXX XXXXXXXXX</p>
+                                <p>IMUYA Youth President</p>
+                            </div>
                     </div>
-                    <div className="mx-w-sm mx-auto border rounded-lg shadow-lg shadow-purple-400">
-                        <img src="/avatar.svg" />
-                        <div  className="p-4">
-                            <p>xxxxxxxx xxxxxxxxx</p>
-                            <p>Vice President 1</p>
+                    <div className="w-full max-w-md mx-auto mt-8 px-4 space-y-32 rounded-lg border shadow-xl shadow-purple-300">
+                        <div className="relative">
+                            <div className="absolute w-36 h-36 bg-white border-l border-purple-600 rounded-full -left-8 -top-8 flex items-center justify-center shadow-md">
+                                <div className="w-32 h-32 rounded-full">
+                                    <img src="/avatar.svg"/>
+                                </div>
+                            </div>
                         </div>
+                        <div className="p-4 text-center">
+                                <p>XXXXXX XXXXXXXXX</p>
+                                <p>Vice President 1</p>
+                            </div>
                     </div>
-                    <div className="mx-w-sm mx-auto border rounded-lg shadow-lg shadow-purple-400">
-                        <img src="/avatar.svg" />
-                        <div className="p-4">
-                            <p>xxxxxxxx xxxxxxxxx</p>
-                            <p>Vice President 2</p>
+                    <div className="w-full max-w-md mx-auto mt-8 px-4 space-y-32 rounded-lg border shadow-xl shadow-purple-300">
+                        <div className="relative">
+                            <div className="absolute w-36 h-36 bg-white border-l border-purple-600 rounded-full -left-8 -top-8 flex items-center justify-center shadow-md">
+                                <div className="w-32 h-32 rounded-full">
+                                    <img src="/avatar.svg"/>
+                                </div>
+                            </div>
                         </div>
+                        <div className="p-4 text-center">
+                                <p>XXXXXX XXXXXXXXX</p>
+                                <p>Vice President 2</p>
+                            </div>
                     </div>
-                    <div className="mx-w-sm mx-auto border rounded-lg shadow-lg shadow-purple-400">
-                        <img src="/avatar.svg" />
-                        <div  className="p-4">
-                            <p>xxxxxxxx xxxxxxxxx</p>
-                            <p>Secretary General</p>
+                    <div className="w-full max-w-md mx-auto mt-8 px-6 space-y-32 rounded-lg border shadow-xl shadow-purple-300">
+                        <div className="relative">
+                            <div className="absolute w-36 h-36 bg-white border-l border-purple-600 rounded-full -left-8 -top-8 flex items-center justify-center shadow-md">
+                                <div className="w-32 h-32 rounded-full">
+                                    <img src="/avatar.svg"/>
+                                </div>
+                            </div>
                         </div>
+                        <div className="p-4 text-center">
+                                <p>XXXXXX XXXXXXXXX</p>
+                                <p>Secretary General</p>
+                            </div>
                     </div>
-                    <div className="mx-w-sm mx-auto border rounded-lg shadow-lg shadow-purple-400">
-                        <img src="/avatar.svg" />
-                        <div className="p-4">
-                            <p>xxxxxxxx xxxxxxxxx</p>
-                            <p>Asst. Secretary General</p>
+                    <div className="w-full max-w-md mx-auto mt-8 px-6 space-y-32 rounded-lg border shadow-xl shadow-purple-300">
+                        <div className="relative">
+                            <div className="absolute w-36 h-36 bg-white border-l border-purple-600 rounded-full -left-8 -top-8 flex items-center justify-center shadow-md">
+                                <div className="w-32 h-32 rounded-full">
+                                    <img src="/avatar.svg"/>
+                                </div>
+                            </div>
                         </div>
+                        <div className="p-4 text-center">
+                                <p>XXXXXX XXXXXXXXX</p>
+                                <p>Asst. Secretary</p>
+                            </div>
                     </div>
-                    <div className="mx-w-sm mx-auto border rounded-lg shadow-lg shadow-purple-400">
-                        <img src="/avatar.svg" />
-                        <div  className="p-4">
-                            <p>xxxxxxxx xxxxxxxxx</p>
-                            <p>Financial Secretary</p>
+                    <div className="w-full max-w-md mx-auto mt-8 px-6 space-y-32 rounded-lg border shadow-xl shadow-purple-300">
+                        <div className="relative">
+                            <div className="absolute w-36 h-36 bg-white border-l border-purple-600 rounded-full -left-8 -top-8 flex items-center justify-center shadow-md">
+                                <div className="w-32 h-32 rounded-full">
+                                    <img src="/avatar.svg"/>
+                                </div>
+                            </div>
                         </div>
+                        <div className="p-4 text-center">
+                                <p>XXXXXX XXXXXXXXX</p>
+                                <p>Financial Secretary</p>
+                            </div>
                     </div>
-                    <div className="mx-w-sm mx-auto border rounded-lg shadow-lg shadow-purple-400">
-                        <img src="/avatar.svg" />
-                        <div className="p-4">
-                            <p>xxxxxxxx xxxxxxxxx</p>
-                            <p>Treasurer</p>
+                    <div className="w-full max-w-md mx-auto mt-8 px-6 space-y-32 rounded-lg border shadow-xl shadow-purple-300">
+                        <div className="relative">
+                            <div className="absolute w-36 h-36 bg-white border-l border-purple-600 rounded-full -left-8 -top-8 flex items-center justify-center shadow-md">
+                                <div className="w-32 h-32 rounded-full">
+                                    <img src="/avatar.svg"/>
+                                </div>
+                            </div>
                         </div>
+                        <div className="p-4 text-center">
+                                <p>XXXXXX XXXXXXXXX</p>
+                                <p>Treasurer</p>
+                            </div>
                     </div>
-                    <div className="mx-w-sm mx-auto border rounded-lg shadow-lg shadow-purple-400">
-                        <img src="/avatar.svg" />
-                        <div  className="p-4">
-                            <p>xxxxxxxx xxxxxxxxx</p>
-                            <p>PRO 1</p>
+                    <div className="w-full max-w-md mx-auto mt-8 px-6 space-y-32 rounded-lg border shadow-xl shadow-purple-300">
+                        <div className="relative">
+                            <div className="absolute w-36 h-36 bg-white border-l border-purple-600 rounded-full -left-8 -top-8 flex items-center justify-center shadow-md">
+                                <div className="w-32 h-32 rounded-full">
+                                    <img src="/avatar.svg"/>
+                                </div>
+                            </div>
                         </div>
+                        <div className="p-4 text-center">
+                                <p>XXXXXX XXXXXXXXX</p>
+                                <p>P.R.O 1</p>
+                            </div>
                     </div>
-                    <div className="mx-w-sm mx-auto border rounded-lg shadow-lg shadow-purple-400">
-                        <img src="/avatar.svg" />
-                        <div className="p-4">
-                            <p>xxxxxxxx xxxxxxxxx</p>
-                            <p>PRO 2</p>
+                    <div className="w-full max-w-md mx-auto mt-8 px-6 space-y-32 rounded-lg border shadow-xl shadow-purple-300">
+                        <div className="relative">
+                            <div className="absolute w-36 h-36 bg-white border-l border-purple-600 rounded-full -left-8 -top-8 flex items-center justify-center shadow-md">
+                                <div className="w-32 h-32 rounded-full">
+                                    <img src="/avatar.svg"/>
+                                </div>
+                            </div>
                         </div>
+                        <div className="p-4 text-center">
+                                <p>XXXXXX XXXXXXXXX</p>
+                                <p>P.R.O 2</p>
+                            </div>
                     </div>
-                    <div className="mx-w-sm mx-auto border rounded-lg shadow-lg shadow-purple-400">
-                        <img src="/avatar.svg" />
-                        <div  className="p-4">
-                            <p>xxxxxxxx xxxxxxxxx</p>
-                            <p>Social Media Director 1</p>
+                    <div className="w-full max-w-md mx-auto mt-8 px-6 space-y-32 rounded-lg border shadow-xl shadow-purple-300">
+                        <div className="relative">
+                            <div className="absolute w-36 h-36 bg-white border-l border-purple-600 rounded-full -left-8 -top-8 flex items-center justify-center shadow-md">
+                                <div className="w-32 h-32 rounded-full">
+                                    <img src="/avatar.svg"/>
+                                </div>
+                            </div>
                         </div>
+                        <div className="p-4 text-center">
+                                <p>XXXXXX XXXXXXXXX</p>
+                                <p>Social Media Director 1</p>
+                            </div>
                     </div>
-                    <div className="mx-w-sm mx-auto border rounded-lg shadow-lg shadow-purple-400">
-                        <img src="/avatar.svg" />
-                        <div className="p-4">
-                            <p>xxxxxxxx xxxxxxxxx</p>
-                            <p>Social Media Director 2</p>
+                    <div className="w-sm max-w-md mx-auto mt-8 px-6 space-y-32 rounded-lg border shadow-xl shadow-purple-300">
+                        <div className="relative">
+                            <div className="absolute w-36 h-36 bg-white border-l border-purple-600 rounded-full -left-8 -top-8 flex items-center justify-center shadow-md">
+                                <div className="w-32 h-32 rounded-full">
+                                    <img src="/avatar.svg"/>
+                                </div>
+                            </div>
                         </div>
+                        <div className="p-4 text-center">
+                                <p>XXXXXX XXXXXXXXX</p>
+                                <p>Social Media Director 2</p>
+                            </div>
                     </div>
-                    <div className="mx-w-sm mx-auto border rounded-lg shadow-lg shadow-purple-400">
-                        <img src="/avatar.svg" />
-                        <div className="p-4">
-                            <p>xxxxxxxx xxxxxxxxx</p>
-                            <p>Social Media Director 3</p>
+                    <div className="w-sm max-w-md mx-auto mt-8 px-6 space-y-32 rounded-lg border shadow-xl shadow-purple-300">
+                        <div className="relative">
+                            <div className="absolute w-36 h-36 bg-white border-l border-purple-600 rounded-full -left-8 -top-8 flex items-center justify-center shadow-md">
+                                <div className="w-32 h-32 rounded-full">
+                                    <img src="/avatar.svg"/>
+                                </div>
+                            </div>
                         </div>
+                        <div className="p-4 text-center">
+                                <p>XXXXXX XXXXXXXXX</p>
+                                <p>Social Media Director 3</p>
+                            </div>
                     </div>
                 </div>
                 
