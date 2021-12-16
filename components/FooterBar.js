1c1e51ac8554c61a2181b08298af9ee7e91d8e91
Matthew <matgold17@gmail.com>
2021-12-16 19:39:56 +0100

    updated

--- components/FooterBar.js
+++ components/FooterBar.js
@@ -2,7 +2,7 @@ function FooterBar() {
     return (
         <div className="text-center text-gray-300 p-4 bg-gray-900">
            <p>This website is still under construction</p>
-           <p className="text-xs">Developed by Chukwu Matthew (codeMat)</p>
+           <p className="text-xs text-gray-500">Developed by Chukwu Matthew (codeMat)</p>
         </div>
     )
 }
